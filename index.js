201e29212b9f495b7fcde2a1c86fad78c785c1d2
GODAWSKI Francis <Francis.GODAWSKI@VRT.BE>
2014-03-15 18:15:00 +0100

    send to host

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
 var colors=["#FF00FF","#FFFF00","#00FF00","#00FFFF","#0000FF"];
+var players=[0,1,2,3,4];
 var connections=0;
+var host=null;
 io.sockets.on("connection",function(socket){
 
 	console.log("socket");
@@ -21,20 +23,26 @@ io.sockets.on("connection",function(socket){
 			socket.disconnect();
 		}else{
 			socket.clientcolor=colors.shift();
+			socket.playernr=players.shift();
 			socket.emit('ok',{color:socket.clientcolor});
 			connections++;
 		}
 	});
+	socket.on("host",function(){
+		host=socket;
+	})
   	socket.on('my other event', function (data) {
     	console.log(data);
   	});
   	socket.on('up', function(data){
   		console.log(data);
   		console.log("up");
+  		if(host!=null) host.emit('up',{player:socket.playernr});
   	});
   	socket.on('down', function(data){
   		console.log(data);
   		console.log("down");
+  		if(host!=null) host.emit('down',{player:socket.playernr});
   	});
   	socket.on('shoot', function(data){
   		console.log(data);
@@ -43,9 +51,15 @@ io.sockets.on("connection",function(socket){
   	socket.on("disconnect",function(data){
   		if(socket.clientcolor){
   			colors.push(socket.clientcolor);
+  			players.push(socket.playernr);
   			delete socket.clientcolor;
   		}
+  		if(socket==host){
+  			host=null;
+  		}else{
   		connections--;
+  			
+  		}
   	});
 });
 
