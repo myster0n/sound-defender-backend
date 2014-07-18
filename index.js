a00dcd464c059524653ffb6c934c35ec451efc84
Lennart <coopmansl@gmail.com>
2014-07-18 12:01:18 +0200

    ne nog betere merge

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var http = require('http');
+var crypto = require('crypto');
+var validator = require('validator');
+var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
 var app = express();
 
 app.use(express.static('../sound-defender'));
@@ -14,8 +18,11 @@ var host=null;
 var adminvars=null;
 // var players=[];
 
+var cryptoKey = "CHANGE_ME";
+var scoreDBFile = "scores.db";
 var nrOfPlayers = 4;
 
+var scoreDB = new sqlite3.Database(scoreDBFile);
 var countDownTime = 30000;
 var startGameCountDown;
 var pinCode;
@@ -29,6 +36,58 @@ function Player(id, socket) {
 var players = new Array(nrOfPlayers);
 for (var i=0; i<nrOfPlayers; i++) {
 	players[i] = new Player(i);
+}
+
+scoreDB.serialize(function() {
+	scoreDB.run("create table if not exists scores (email TEXT, score TEXT)");
+});
+
+app.use(express.urlencoded())
+app.use(express.json())
+app.post('/addscore', function(req, res) {
+	if (!req.param('score') || !req.param('email')) {
+		res.send(400, 'Score of email adres niet meegegeven!');
+		res.end();
+		return;
+	}
+
+	var score=0;
+	try {
+		score = decryptScore(req.param('score'));
+		if (!validator.isInt(score)) {
+			throw new Error('Score klopt niet!');
+		}
+
+	} catch (err) {
+		res.send(400, 'Score klopt niet!');
+		res.end();
+		return;
+	}
+
+	var email=req.param('email');
+	if (!validator.isEmail(email)) {
+		res.send(400, 'email adres klopt niet!');
+		res.end();
+		return;
+	}
+
+	var stmt = scoreDB.prepare("INSERT INTO scores VALUES (?, ?)");
+	stmt.run(score, email);
+	stmt.finalize();
+
+	console.log("score: "+score+" email: "+email);
+	res.send(200, 'Uw score werd opgeslaan. Bedankt!');
+	res.end();
+});
+
+function decryptScore(input) {
+	var decipher = crypto.createDecipher('aes256', cryptoKey);
+	return decipher.update(input, 'hex', 'utf8') + decipher.final('utf8');
+}
+
+function encryptScore(score) {
+	var cipher = crypto.createCipher('aes256', cryptoKey);
+	return cipher.update(score, 'utf8', 'hex') + cipher.final('hex');
 }
 
 io.sockets.on("connection",function(socket){
